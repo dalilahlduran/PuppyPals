9eafd738ebf825031bbb85ec69b7189cddaa2d69
Dalilah Duran <dalilahduran@Dalilahs-iMac.local>
2024-06-05 20:14:58 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import './App.css'
+// import './App.css'
+import { useState } from 'react'
+
+import {puppyList} from './data.js'
+
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
+
   return (
-    <>
-      <div>
-      </div>
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
